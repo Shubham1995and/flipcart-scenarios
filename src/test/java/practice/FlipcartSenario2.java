e842dffa11fb804b27ed8f5e7f918ad13de15ee5
Lenovo <Lenovo@192.168.1.99>
2022-09-27 20:08:40 -0700

    first commit

--- src/test/java/practice/FlipcartSenario2.java
+++ src/test/java/practice/FlipcartSenario2.java
@@ -1,10 +1,29 @@
 package practice;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FlipcartSenario2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("WebDriver.chrome.driver","C:\\Users\\Lenovo\\eclipse-workspace\\vtiger\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.xpath("//button[text()='X'")).click();
+		driver.findElement(By.name("q")).sendKeys("redmi");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		List<WebElement> REDMI = driver.findElements(By.xpath("//li[@class='rgWa7D'and (text()='4GB RAM|64GB ROM | Expandable Upto 1TB')]"));
+        for(WebElement st : REDMI)
+        {
+        	System.out.println(st.getText());
+        }
 	}
 
 }
